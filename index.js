e8ed1fbfe02eab9f8f4e0c0cab6cf1144444ccb9
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-04 16:44:44 +0300

    13 done

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ console.log(testArray(gg));
 10)
 
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3 */
-function testArray(x) {
+function testArray2(x) {
     if (x[0] === 1 || x[1] === 3) {
         return "contains 1 or 3";
     } else if (x[0] === 3 || x[1] === 1) {
@@ -125,58 +125,75 @@ function testArray(x) {
         return "doesnt contain 1 or 3";
     }
 }
-console.log(testArray(gg));
-/*11)
+console.log(testArray2(gg));
+// 11)
 
-Create a function to find the longest string from a given array of strings.
+// Create a function to find the longest string from a given array of strings.
+let stringUno = "the first string here";
+let stringDuos = "the second string right here";
+let stringTres = "the third string thats not actually here";
+function theLongestStringHere(a, b, c) {
+    console.log(Math.max(a.length, b.length, c.length));
+}
+theLongestStringHere(stringUno, stringDuos, stringTres);
+function theLongestStringHere2(a, b, c) {
+    if (a.length > b.length && a.length > c.length) {
+        return a;
+    } else if (b.length > a.length && b.length > c.length) {
+        return b;
+    } else {
+        return c;
+    }
+}
+console.log(`The longest string is ${theLongestStringHere2(stringUno, stringDuos, stringTres)}`);
+// 12)
 
-12)
+// Create a function to find the types of a given angle.
+//sorry i dont get it
 
-Create a function to find the types of a given angle.
+// Types of angles:
+//     Acute angle: An angle between 0 and 90 degrees.
+//     Right angle: An 90 degree angle.
+//     btuse angle: An angle between 90 and 180 degrees.
+//     Straight angle: A 180 degree angle.
 
-Types of angles:
-    Acute angle: An angle between 0 and 90 degrees.
-    Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.
+// 13)
+// Create a function to find the index of the greatest element of a given array of integers
+let lol = [3, 6, 9];
+function findPosition(o) {
+    //the three dots are the spread syntax, i guess sometims allows going through an array without loop.
+    let biggest = Math.max(...o);
+    return o.indexOf(biggest);
+}
+console.log(findPosition(lol));
+// 14)
+// Create a function to get the largest even number from an array of integers.
 
-13)
+//where is 15????
 
-Create a function to find the index of the greatest element of a given array of integers
+// 16)
+// Create a function to check from two given integers, whether one is positive and another one is negative.
 
-14)
+// 17)
+// Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case.
 
-Create a function to get the largest even number from an array of integers.
+// 18)
+// Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
 
-16)
+// 19)
+// Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
 
-Create a function to check from two given integers, whether one is positive and another one is negative.
+// If the number has 3 as a factor, output 'Diego'.
+// If the number has 5 as a factor, output 'Riccardo'.
+// If the number has 7 as a factor, output 'Stefano'.
+// If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+// Examples
+// 28's factors are 1, 2, 4, 7, 14, 28.
+// this would be a simple "Stefano".
+// 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+// this would be a "DiegoRiccardo".
+// 34 has four factors: 1, 2, 17, and 34.
+// this would be "34".
 
-17)
-
-Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
-
-18)
-
-Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
-
-19)
-
-Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
-
-If the number has 3 as a factor, output 'Diego'.
-If the number has 5 as a factor, output 'Riccardo'.
-If the number has 7 as a factor, output 'Stefano'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-Examples
-28's factors are 1, 2, 4, 7, 14, 28.
-this would be a simple "Stefano".
-30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-this would be a "DiegoRiccardo".
-34 has four factors: 1, 2, 17, and 34.
-this would be "34".
-
-20)
-Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
-
-*/
+// 20)
+// Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
